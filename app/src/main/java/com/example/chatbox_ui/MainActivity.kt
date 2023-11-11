f2d7d13e3b83d42be3abbb1a349cd523783ea6e9
ChibuikeHenry <ugwuhenry195@gmail.com>
2023-11-11 23:16:57 +0100

    .
    commit

---
+++ app/src/main/java/com/example/chatbox_ui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.chatbox_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatbotui.adapters.DMsAdapter
+import com.example.chatbotui.adapters.UserAdapter
+import com.example.chatbotui.data.DataSource
+import com.example.chatbox_ui.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val users = DataSource().users()
+        val dms = DataSource().userMessages()
+
+        binding.apply {
+            rvHorizontal.adapter = UserAdapter(this@MainActivity, users)
+            rvVertical.adapter = DMsAdapter(this@MainActivity, dms)
+
+            rvHorizontal.setHasFixedSize(true)
+            rvVertical.setHasFixedSize(true)
+        }
+
+    }
+
+
+}
